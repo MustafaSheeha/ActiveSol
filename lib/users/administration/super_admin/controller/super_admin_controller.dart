0fd1a4488da78ccb5c431417d8f183407b7fc1e2
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-12-05 21:45:22 +0200

    Refactor SuperAdminController to use AdminRepository
    Updated SuperAdminController to utilize AdminRepository for admin management operations, removing direct FirestoreService calls and simplifying the controller logic. Implemented methods to add, delete, get by name, get all, and update admins using the repository.

--- lib/users/administration/super_admin/controller/super_admin_controller.dart
+++ lib/users/administration/super_admin/controller/super_admin_controller.dart
@@ -1,5 +1,3 @@
-import 'package:active_sol_app/core/database/remote/firebase/firestore_service.dart';
-import 'package:active_sol_app/core/enums.dart';
 import 'package:active_sol_app/core/models/user.dart';
 import 'package:active_sol_app/core/repository/admin_repository.dart';
 import 'package:active_sol_app/users/administration/admin/model/admin.dart';
@@ -8,54 +6,28 @@ import 'package:get/get.dart';
 
 class SuperAdminController extends GetxController
     implements SuperAdminBehavior {
-  final FirestoreService _firestoreService = FirestoreService();
   @override
   Future<void> addAdmin(Admin admin) async {
-    try {
-      // get last Id
-      int updatedId = await FirestoreService().getlastId('admin') + 1;
-      // Step 1: Generate a unique ID for the admin
-      admin.id = await admin.generateId('admin');
-
-      // Step 2: Prepare the admin data for saving
-      final adminData = admin.toJson();
-
-      // Step 3: Save the admin data in the Firebase database
-      await _firestoreService.create('admins', adminData);
-      await _firestoreService.updatelastId('admin', updatedId);
-
-      print('Admin added successfully with ID: ${admin.id}');
-    } catch (e) {
-      print('Error adding admin: $e');
-      throw Exception('Failed to add admin: $e');
-    }
-  }
-
-  @override
-  void assignRoles() {
-    // TODO: implement assignRoles
+    AdminRepository.addAdmin(admin);
   }
 
   @override
   Future<void> deleteAdmin(String id) {
-    // TODO: implement deleteAdmin
-    throw UnimplementedError();
+    return AdminRepository.deleteAdmin(id);
   }
 
   @override
-  User? getAdminById(String id) {
-    // TODO: implement getAdminById
-    throw UnimplementedError();
+  Future<Admin> getAdminByName(String adminName) {
+    return AdminRepository.getAdminByName(adminName);
   }
 
   @override
- Future<List<Admin>> getAllAdmins()async {
+  Future<List<Admin>> getAllAdmins() async {
     return await AdminRepository.getAllAdmins();
   }
 
   @override
-  Future<void> updateAdmin(User updatedAdmin) {
-    // TODO: implement updateAdmin
-    throw UnimplementedError();
+  Future<void> updateAdmin(Admin updatedAdmin) async {
+    return await AdminRepository.updateAdmin(updatedAdmin);
   }
 }
