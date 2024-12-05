aa571cfbf5e8cb55eab5c547200ed8903075b49f
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-12-05 20:17:25 +0200

    Change administrationRole type to String
    Update the `administrationRole` field in the `Administration` class from `AdministrationRole` to `String` to accommodate more flexible role definitions. Adjust the constructor to reflect the change in field type.

--- lib/users/administration/model/administration.dart
+++ lib/users/administration/model/administration.dart
@@ -2,9 +2,9 @@ import 'package:active_sol_app/core/enums.dart';
 import 'package:active_sol_app/core/models/user.dart';
 
 abstract class Administration extends User {
-  AdministrationRole administrationRole;
+  String administrationRole;
   Administration({
-    required super.id,
+    super.id,
     required super.name,
     required super.phone,
     required this.administrationRole,
