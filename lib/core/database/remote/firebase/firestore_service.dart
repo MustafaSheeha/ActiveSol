2e2d42fe63eac0c2f4611f4c4b34530f25866e3f
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-12-05 19:22:27 +0200

    Add  get last ID method to class FirestoreService

--- lib/core/database/remote/firebase/firestore_service.dart
+++ lib/core/database/remote/firebase/firestore_service.dart
@@ -3,27 +3,39 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService implements DatabaseService {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  Future<int> getlastId(String prefix) => firestore
+      .collection('ids')
+      .doc('2024')
+      .collection('12')
+      .doc(prefix)
+      .get()
+      .then((value) => value['lastId']);
 
   @override
   Future<void> create(String collection, Map<String, dynamic> data) async {
     await firestore.collection(collection).add(data);
   }
-@override
+
+  @override
   Future<Map<String, dynamic>?> read(String collection, String id) async {
     final docSnapshot = await firestore.collection(collection).doc(id).get();
     return docSnapshot.exists ? docSnapshot.data() : null;
   }
+
   @override
-  Future<void> update(String collection, String id, Map<String, dynamic> data) async {
+  Future<void> update(
+      String collection, String id, Map<String, dynamic> data) async {
     await firestore.collection(collection).doc(id).update(data);
-  } 
+  }
+
   @override
   Future<void> delete(String collection, String id) async {
     await firestore.collection(collection).doc(id).delete();
   }
 
   @override
-  Future<List<Map<String, dynamic>>> query(String collection, {Map<String, dynamic>? filters}) async {
+  Future<List<Map<String, dynamic>>> query(String collection,
+      {Map<String, dynamic>? filters}) async {
     Query query = firestore.collection(collection);
     if (filters != null && filters.isNotEmpty) {
       filters.forEach((key, value) {
@@ -31,10 +43,8 @@ class FirestoreService implements DatabaseService {
       });
     }
     final querySnapshot = await query.get();
-    return querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+    return querySnapshot.docs
+        .map((doc) => doc.data() as Map<String, dynamic>)
+        .toList();
   }
-
-  
-
-   
 }
