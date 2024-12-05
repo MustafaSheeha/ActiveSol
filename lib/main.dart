0bf661cfeb811a8c0e35ce7ed2380d491ae9a206
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-12-05 16:49:47 +0200

    Initialize services before running the app
    Ensure that all necessary services are initialized before the app starts by calling `initServices()` and making the `main` function asynchronous.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:active_sol_app/core/functions/init_services.dart';
 import 'package:active_sol_app/my_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initServices();
   runApp(const MyApp());
 }
